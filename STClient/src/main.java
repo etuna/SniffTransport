3a98a98d9a3edf1a310804f47b76473e651c9e77
etuna <etuna@ku.edu.tr>
2018-11-20 11:21:56 +0300

    first commit

---
+++ STClient/src/main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Client client = new Client("localhost");
+	}
+
+}
